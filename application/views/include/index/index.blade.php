65b76f9dc539ef709d53f08b2cbf0e8121099716
lytranuit <lytranuit@gmail.com>
2020-06-06 08:32:40 +0700

    fix bug

--- application/views/include/index/index.blade.php
+++ application/views/include/index/index.blade.php
@@ -12,7 +12,7 @@
                             @foreach($topics as $row)
                             <div class="item m-2">
                                 <a class="" href="{{base_url()}}index/category/{{$row->id}}">
-                                    <img class="img-responsive" src="@if(isset($row->image)){{$row->image->src}}@endif" alt="">
+                                    <img class="img-responsive" src="@if(isset($row->image)){{$row->image->src}}@endif" alt="" height="250">
                                     <div class="caption text-center">
                                         <p>
                                             {{ $row->{pick_language($row,'name_')} }}
