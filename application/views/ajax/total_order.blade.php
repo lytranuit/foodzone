514cee718d07e25e15e01449a0c7f74cb8271e88
tran <lytranuit@gmail.com>
2020-07-18 23:41:05 +0700

    update

--- application/views/ajax/total_order.blade.php
+++ application/views/ajax/total_order.blade.php
@@ -33,7 +33,7 @@
                     {{number_format($cart['paid_amount'],0,",",".")}}đ
                 </span>
 
-                <div>{{NumberToTextVN($cart['paid_amount'])}}</div>
+                <div>{{NumberToText($cart['paid_amount'])}}</div>
             </td>
         </tr>
         <tr>
