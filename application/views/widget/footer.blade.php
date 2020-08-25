6162928157c749d3a31d2bfc88b948b61269f422
tran <lytranuit@gmail.com>
2020-08-25 23:47:06 +0700

    update language

--- application/views/widget/footer.blade.php
+++ application/views/widget/footer.blade.php
@@ -59,3 +59,4 @@
     </div>
 
 </footer>
+<div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
