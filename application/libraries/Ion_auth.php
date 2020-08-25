301695991cf19edcf3bd956707c9bb04dba52378
tran <lytranuit@gmail.com>
2020-08-25 22:35:38 +0700

    update language

--- application/libraries/Ion_auth.php
+++ application/libraries/Ion_auth.php
@@ -151,6 +151,9 @@ class Ion_auth
                     $this->set_message('forgot_password_successful');
                     return $data;
                 } else {
+                    echo "<pre>";
+                    print_r($conf);
+                    die();
                     $email_config = array(
                         'mailtype' => 'html',
                         'protocol' => "smtp",
