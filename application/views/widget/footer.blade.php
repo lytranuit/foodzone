17a3242fbe615c85a617158384c8f42aae5becc6
IT-PC01\Administrator <lytranuit@gmail.com>
2021-05-31 18:45:32 +0700

    bct

--- application/views/widget/footer.blade.php
+++ application/views/widget/footer.blade.php
@@ -8,8 +8,7 @@
                     </a>
                     <p class="header_footer">{{$options['company_name']}}</p>
                     <p>{{lang("footer_tru_so")}}: <span class="text-dark">{{$options['dia_chi']}}</span></p>
-                    <p>{{lang("footer_phone")}}: <a class="text-dark"
-                            href="tel:{{$options['hot_line']}}">{{$options['hot_line']}}</span></a></p>
+                    <p>{{lang("footer_phone")}}: <a class="text-dark" href="tel:{{$options['hot_line']}}">{{$options['hot_line']}}</span></a></p>
                     <p>Giấy chứng nhận Đăng ký Kinh doanh số 0303582244 do Phòng Đăng Ký Kinh Doanh - Kế hoạch và Đầu tư
                         Thành phố Hồ Chí Minh cấp đăng ký thay đổi lần thứ 11 ngày 07/02/2020</p>
                 </div>
@@ -32,12 +31,9 @@
                 </div>
                 <div class="col-12 col-lg-3">
                     <ul class="list-inline" style="font-size: 25px;">
-                        <li><a target="_blank" class="icon icon-xxs-mod-1 fab fa-facebook icon-facebook"
-                                href="http://www.facebook.com/sharer/sharer.php?u={{current_url()}}"></a></li>
-                        <li><a target="_blank" class="icon icon-xxs-mod-1 fab fa-twitter icon-twitter"
-                                href="https://twitter.com/intent/tweet?text=Link&url={{current_url()}}"></a></li>
-                        <li class="zalo-share-button" data-href="" data-oaid="579745863508352884" data-layout="2"
-                            data-color="blue" data-customize=true>
+                        <li><a target="_blank" class="icon icon-xxs-mod-1 fab fa-facebook icon-facebook" href="http://www.facebook.com/sharer/sharer.php?u={{current_url()}}"></a></li>
+                        <li><a target="_blank" class="icon icon-xxs-mod-1 fab fa-twitter icon-twitter" href="https://twitter.com/intent/tweet?text=Link&url={{current_url()}}"></a></li>
+                        <li class="zalo-share-button" data-href="" data-oaid="579745863508352884" data-layout="2" data-color="blue" data-customize=true>
                             <a href='#' class="icon icon-xxs-mod-1 zalo">
                                 <i></i>
                             </a>
@@ -50,6 +46,12 @@
                     <div class="customer">
                         <span>{{lang("footer_customer")}}: </span>
                         <span class="phone text-dark">{{$options['hot_line']}}</span>
+                    </div>
+                    <div>
+                        <a href="http://online.gov.vn/HomePage/CustomWebsiteDisplay.aspx?DocId=52565" target="_blank" class="bocongthuong">
+                            <div class="text-dark">Chứng nhận SGD TMĐT</div>
+                            <img src="public/image/bct.png" alt="TMĐT">
+                        </a>
                     </div>
                 </div>
             </div>
@@ -64,25 +66,25 @@
     </div>
 
 </footer>
-<div class="zalo-chat-widget" data-oaid="2343818110642891994" data-welcome-message="Rất vui khi được hỗ trợ bạn!"
-    data-autopopup="0" data-width="350" data-height="420"></div>
+<div class="zalo-chat-widget" data-oaid="2343818110642891994" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
 <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
 <script>
     window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v9.0'
-    });
-  };
+        FB.init({
+            xfbml: true,
+            version: 'v9.0'
+        });
+    };
 
-  (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 </script>
 <style>
     #fb-root iframe {
